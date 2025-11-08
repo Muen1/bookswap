0494555986f9987649cb272d4808f8b287e7296c
Muen1 <c.mutie@alustudent.com>
2025-11-09 01:48:46 +0200

    create firebase_options.dart

---
+++ lib/firebase_options.dart
@@ -0,0 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    return const FirebaseOptions(
+      apiKey: 'AIzaSyAs6GjdUthii4Wf21aLVUVZHYA99oQxdwY',
+      appId: '1:879455539893:android:ea9c550cfb35ad5008e4fc',
+      messagingSenderId: '879455539893',
+      projectId: 'bookswap-26ed4',
+      storageBucket: 'bookswap-26ed4.firebasestorage.app',
+      androidClientId: '', 
+      iosClientId: '', 
+    );
+  }
+}
