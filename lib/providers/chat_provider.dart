e6acc8a16c8567af30f6c9345d54dd7840746268
Muen1 <c.mutie@alustudent.com>
2025-11-09 04:34:21 +0200

    Create Riverpod providers for chat functionality

---
+++ lib/providers/chat_provider.dart
@@ -0,0 +1,15 @@
+import 'package:bookswap/models/chart.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../services/chat_service.dart';
+
+final chatServiceProvider = Provider<ChatService>((ref) => ChatService());
+
+final userChatRoomsProvider = StreamProvider.family<List<ChatRoom>, String>((ref, userId) {
+  final service = ref.read(chatServiceProvider);
+  return service.getUserChatRooms(userId);
+});
+
+final chatMessagesProvider = StreamProvider.family<List<ChatMessage>, String>((ref, chatRoomId) {
+  final service = ref.read(chatServiceProvider);
+  return service.getChatMessages(chatRoomId);
+});
