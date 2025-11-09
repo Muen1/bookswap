8d494b4e5f22bc26ad665ccca60a4b0eda785854
Muen1 <c.mutie@alustudent.com>
2025-11-09 05:39:17 +0200

    Update app to handle FCM tokens on login

--- lib/app.dart
+++ lib/app.dart
@@ -5,16 +5,44 @@ import 'screens/auth_screen.dart';
 import 'screens/home_screen.dart';
 import 'screens/email_verification_screen.dart';
 
-class App extends ConsumerWidget {
+class App extends ConsumerStatefulWidget {
   const App({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<App> createState() => _AppState();
+}
+
+class _AppState extends ConsumerState<App> {
+  @override
+  void initState() {
+    super.initState();
+    _setupFCMToken();
+  }
+
+  Future<void> _setupFCMToken() async {
+    // Wait for auth state to be available
+    await Future.delayed(const Duration(seconds: 2));
+    
+    final authService = ref.read(authServiceProvider);
+    final currentUser = ref.read(authStateProvider).value;
+    
+    if (currentUser != null) {
+      await authService.updateUserFCMToken(currentUser.uid);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final authState = ref.watch(authStateProvider);
 
     return authState.when(
       data: (user) {
         if (user != null) {
+          // Update FCM token when user logs in
+          WidgetsBinding.instance.addPostFrameCallback((_) {
+            ref.read(authServiceProvider).updateUserFCMToken(user.uid);
+          });
+
           if (!user.emailVerified) {
             return EmailVerificationScreen(user: user);
           }
