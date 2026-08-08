6cb013600dfa060856fbecd0f4bc744abb05f241
Mueni <c.mutie@alustudent.com>
2026-08-08 02:03:05 +0200

    Add AppColors class for centralized color management

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color paper = Color(0xFFF3EDE0);
+  static const Color spineGreen = Color(0xFF2B4735);
+  static const Color inkNavy = Color(0xFF1B2430);
+  static const Color mustard = Color(0xFFC08A28);
+  static const Color mutedText = Color(0xFF6B6355);
+  static const Color cardSurface = Color(0xFFFAF6EC);
+}
